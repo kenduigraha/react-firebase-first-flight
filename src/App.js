48fe901166272cd825418eafc18b1e3205c037ff
kenduigraha <ken.duigraha@kudo.co.id>
2017-04-12 15:35:45 +0700

    checkpoint setting firebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import dotenv from 'dotenv';
+dotenv.config({ silent: true });
 import React, { Component } from 'react';
+import * as firebase from 'firebase';
+
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    let config = {
+        apiKey: process.env.REACT_APP_firebase_apiKey,
+        authDomain: process.env.REACT_APP_firebase_authDomain,
+        databaseURL: process.env.REACT_APP_firebase_databaseURL,
+        projectId: process.env.REACT_APP_firebase_projectId,
+        storageBucket: process.env.REACT_APP_firebase_storageBucket,
+        messagingSenderId: process.env.REACT_APP_firebase_messagingSenderId,
+      };
+
+    firebase.initializeApp(config);
+    let bigOne = document.getElementById('bigOne');
+    let dbRef = firebase.database().ref().child('text');
+    dbRef.on('value', snap => bigOne.innerText = snap.val());
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,6 +31,8 @@ class App extends Component {
         <pre className="App--data">
           One day, some data from Firebase will go here.
         </pre>
+
+        <hi id="bigOne"></hi>
       </div>
     );
   }
